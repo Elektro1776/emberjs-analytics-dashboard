6ba40b4ba27ef23c642d1bb8a24f963048d7238e
Paul Dickerson <pdickerson@clearleap.com>
2015-08-10 22:00:03 -0400

    Figured out how to update the model and apply updates to charts after ajax call.

--- app/components/refresh-chart.js
+++ app/components/refresh-chart.js
@@ -5,7 +5,7 @@ export default Ember.Component.extend({
   classNames: 'btn-no-style pull-right refresh',
   click: function(){
     var ref = this;
-    Ember.$.getJSON('/JSON/update.json').then(function(r) {
+    Ember.$.getJSON('/JSON/updateBar.json').then(function(r) {
       ref.sendAction('updateChartData', r)
     });
   }
--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,9 +1,13 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 
-export default Ember.ArrayController.extend({
+export default Ember.Controller.extend({
   sortProperties: ['id:asc'],
   chartData:  Ember.computed('model', function(){
     return this.get('model').mapBy('chartData')
+  }),
+  title: Ember.computed('model', function(){
+    return this.get('model').mapBy('title')
   }),
   options: Ember.computed('model', function(){
     return {
@@ -16,19 +20,40 @@ export default Ember.ArrayController.extend({
   }),
   actions: {
     setChartData: function(data){
-      var model = this.get('model');
-      var title = data.title;
-      var chartData = data.chartData;
+      var self = this;
+      var chartPromise = this.store.find('application', data.id);
+      chartPromise.then(function(application){
+        application.set('title', data.title);
+        application.set('chartData', data.chartData);
+      });
+      //
+      // var chart = this.get('model');
+      // var title = data.title;
+      // var chartData = data.chartData;
+      //
+      // var newD = {
+      //   labels: ["toast", "butter", "March", "April", "May", "June", "July"],
+      //   datasets: [
+      //       {
+      //           label: "Active Plays",
+      //           data: [0, 200, 0, 175, 238, 300, 560]
+      //       },
+      //       {
+      //           label: "Abandoned Plays",
+      //           data: [10, 20, 50, 300, 30, 80, 12]
+      //       }
+      //   ]
+      // }
 
-      model.set('title', title);
-      model.set('chartData', chartData);
+      // this.set('title', title);
+      //console.log(this.get('title'));
 
-      console.log(model);
+      // chart.save();
 
-      var record = this.store.find('application', data.id);
-      record.get('title', title);
-      record.get('chartData', chartData);
-      console.log(record);
+      // var record = this.store.find('application', data.id);
+      // record.get('title', title);
+      // record.get('chartData', chartData);
+      // console.log(record);
       // console.log(data.chartData);
       // record.set('title', title);
       // record.set('chartData', chartData);
